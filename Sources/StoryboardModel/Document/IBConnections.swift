4aa3d0000e2ce5a3a7e0362ab9aae2b6cfaf2517
Christophe Bronner <christophe.bronner@videotron.ca>
2025-07-25 16:03:11 -0400

    IBConnections

---
+++ Sources/StoryboardModel/Document/IBConnections.swift
@@ -0,0 +1,35 @@
+//
+//  IBConnections.swift
+//  storyboard2code
+//
+//  Created by Christophe Bronner on 2025-07-16.
+//
+
+import SwiftXML
+
+public struct IBOutlet: Codable {
+	@XMLAttribute public var id: String
+	@XMLAttribute public var property: String
+	@XMLAttribute public var destination: String
+}
+
+public struct IBOutletCollection: Codable {
+	@XMLAttribute public var id: String
+	@XMLAttribute public var property: String
+	@XMLAttribute public var destination: String
+	@XMLAttribute public var collectionClass: String
+}
+
+public struct IBAction: Codable {
+	@XMLAttribute public var id: String
+	@XMLAttribute public var selector: String
+	@XMLAttribute public var eventType: String
+	@XMLAttribute public var destination: String
+}
+
+public struct IBSegue: Codable {
+	@XMLAttribute public var id: String
+	@XMLAttribute public var kind: String
+	@XMLAttribute public var destination: String
+	@XMLAttribute public var relationship: String
+}
